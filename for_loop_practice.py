3dcef4b6fdaa805d207b9ac8f4c58ebc598d7b7a
Jitendra Maurya <157475699+jit8899@users.noreply.github.com>
2024-02-05 17:04:31 +0530

    Add files via upload

---
+++ for_loop_practice.py
@@ -0,0 +1,15 @@
+'''l1 = ["sudh","jit","mohan","sonu"]
+for i in l1:
+ if i=="jit":
+     break
+print(i)'''
+
+# continue 
+
+l1 = ['sudh', 'jit', 'mohan', 'sonu']
+for i in l1:
+    if i == 'jit':
+        continue
+    print(i)
+
+
